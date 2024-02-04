de76445af1430356b5f55fedb51bcb4bb7eda637
DESKTOP-9VU9I7S\Rai <ajipunk008@gmail.com>
2024-02-05 05:57:48 +0700

    add some comment on auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -17,20 +17,26 @@ export class AuthService {
   async signIn(signInDto: SigninDto): Promise<{ access_token: string }> {
     const { email, username, password } = signInDto;
 
+    // If both email and username are not provided, throw an error
     if (!email && !username) {
       throw new UnauthorizedException();
     }
 
     let user: User | null = null;
     if (email) {
+      // If email is provided, find the user by email
       user = await this.usersService.findByEmail(email);
     } else if (username) {
+      // If username is provided, find the user by username
       user = await this.usersService.findByUsername(username);
     }
 
+    // If password is incorrect, throw an error
     if (user?.password !== password) {
       throw new UnauthorizedException();
     }
+
+    // return the access token
     const payload = { sub: user.id, username: user.username, firstName: user.firstName, lastName: user.lastName };
     return {
       access_token: await this.jwtService.signAsync(payload),
@@ -38,7 +44,10 @@ export class AuthService {
   }
 
   async signUp(registerDto: RegisterDto): Promise<User> {
+    // Create a new user
     const user = await this.usersService.create(registerDto);
+
+    // Send a welcome email
     this.eventEmitter.emit('user.registered', { email: user.email, name: `${user.firstName} ${user.lastName}` });
     return user;
   }
